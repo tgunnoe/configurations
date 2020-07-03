90c3e064f2822e62ed5f88a6cceadd5b9b81f5a1
tgunnoe <t@gvno.net>
2020-07-03 06:24:25 -0400

    Fix paste in emacs, WIP copy-cut, other settings

--- modules/emacs-init.nix
+++ modules/emacs-init.nix
@@ -127,6 +127,13 @@
                 (lambda ()
                   (when (not (derived-mode-p 'ein:notebook-multilang-mode))
 	              (delete-trailing-whitespace))))
+
+      ;; Check (on save) whether the file edited contains a shebang, if yes,
+      ;; make it executable from
+      ;; http://mbork.pl/2015-01-10_A_few_random_Emacs_tips
+      (add-hook 'after-save-hook #'executable-make-buffer-file-executable-if-script-p)
+
+
       ;; Auto-wrap at 80 characters
       (setq-default auto-fill-function 'do-auto-fill)
       (setq-default fill-column 80)
@@ -134,6 +141,55 @@
 
       ;; Disable auto-fill-mode in programming mode
       (add-hook 'prog-mode-hook (lambda () (auto-fill-mode -1)))
+
+      ;; Non-nil means draw block cursor as wide as the glyph under it.
+      ;; For example, if a block cursor is over a tab, it will be drawn as
+      ;; wide as that tab on the display.
+      (setq x-stretch-cursor t)
+
+      ;; Overwrite region selected
+      (delete-selection-mode t)
+
+
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;; Enable terminal emacs to copy and paste from system clipboard
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;; Note: this uses C-c before the usual C-w, M-w, and C-y
+      ;; From: https://stackoverflow.com/questions/64360/how-to-copy-text-from-emacs-to-another-application-on-linux
+;; FIXME, COPY and CUT need extra work
+(defun my-copy-to-xclipboard(arg)
+  (interactive "P")
+  (cond
+   ((not (use-region-p))
+    (message "Nothing to yank to X-clipboard"))
+   ((and (not (display-graphic-p))
+         (/= 0 (shell-command-on-region
+                (region-beginning) (region-end) "wl-copy")))
+    (message "Error: Is program `xsel' installed?"))
+   (t
+    (when (display-graphic-p)
+      (call-interactively 'clipboard-kill-ring-save))
+    (message "Yanked region to X-clipboard")
+    (when arg
+      (kill-region  (region-beginning) (region-end)))
+    (deactivate-mark))))
+
+(defun my-cut-to-xclipboard()
+  (interactive)
+  (my-copy-to-xclipboard t))
+
+(defun my-paste-from-xclipboard()
+  (interactive)
+  (if (display-graphic-p)
+      (clipboard-yank)
+    (insert (shell-command-to-string "wl-paste"))))
+
+(global-set-key [(control shift x)] 'my-cut-to-xclipboard)
+(global-set-key [(control shift c)] 'my-copy-to-xclipboard)
+(global-set-key [(control shift v)] 'my-paste-from-xclipboard)
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
     '';
 
@@ -403,18 +459,19 @@
       config = "(winner-mode 1)";
     };
 
-    writeroom-mode = {
-      enable = true;
-      command = [ "writeroom-mode" ];
-      bind = {
-        "M-[" = "writeroom-decrease-width";
-        "M-]" = "writeroom-increase-width";
-      };
-      hook = [ "(writeroom-mode . visual-line-mode)" ];
-      config = ''
-          (setq writeroom-bottom-divider-width 0)
-        '';
-    };
+    # This was causing problem with taking C-S-V as M-[
+    # writeroom-mode = {
+    #   enable = true;
+    #   command = [ "writeroom-mode" ];
+    #   bind = {
+    #     "M-[" = "writeroom-decrease-width";
+    #     "M-]" = "writeroom-increase-width";
+    #   };
+    #   hook = [ "(writeroom-mode . visual-line-mode)" ];
+    #   config = ''
+    #       (setq writeroom-bottom-divider-width 0)
+    #     '';
+    # };
 
     buffer-move = {
       enable = true;
@@ -558,7 +615,7 @@
     multiple-cursors = {
       enable = true;
       bind = {
-        "C-S-c C-S-c" = "mc/edit-lines";
+        #"C-S-c C-S-c" = "mc/edit-lines";
         "C-c m" = "mc/mark-all-like-this";
         "C->" = "mc/mark-next-like-this";
         "C-<" = "mc/mark-previous-like-this";
@@ -1388,6 +1445,63 @@
         "C-c t f" = "treemacs-find-file";
         "C-c t t" = "treemacs";
       };
+    };
+    powerline = {
+      enable = true;
+      config = ''
+          (defun powerline-right-theme ()
+     "Setup a mode-line with major and minor modes on the right side."
+     (interactive)
+     (setq-default mode-line-format
+                   '("%e"
+                     (:eval
+                      (let* ((active (powerline-selected-window-active))
+                             (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                             (mode-line (if active 'mode-line 'mode-line-inactive))
+                             (face0 (if active 'powerline-active0 'powerline-inactive0))
+                             (face1 (if active 'powerline-active1 'powerline-inactive1))
+                             (face2 (if active 'powerline-active2 'powerline-inactive2))
+                             (separator-left (intern (format "powerline-%s-%s"
+                                                             (powerline-current-separator)
+                                                             (car powerline-default-separator-dir))))
+                             (separator-right (intern (format "powerline-%s-%s"
+                                                              (powerline-current-separator)
+                                                              (cdr powerline-default-separator-dir))))
+                             (lhs (list (powerline-raw "%*" face0 'l)
+                                        (powerline-buffer-size face0 'l)
+                                        (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
+                                        (powerline-raw " ")
+                                        (funcall separator-left face0 face1)
+                                        (powerline-narrow face1 'l)
+                                        (powerline-vc face1)))
+                             (center (list (powerline-raw global-mode-string face1 'r)
+                                           (powerline-raw "%4l" face1 'r)
+                                           (powerline-raw ":" face1)
+                                           (powerline-raw "%3c" face1 'r)
+                                           (funcall separator-right face1 face0)
+                                           (powerline-raw " ")
+                                           (powerline-raw "%6p" face0 'r)
+                                           (powerline-hud face2 face1)
+                                           ))
+                             (rhs (list (powerline-raw " " face1)
+                                        (funcall separator-left face1 face2)
+                                        (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                          (powerline-raw erc-modified-channels-object face2 'l))
+                                        (powerline-major-mode face2 'l)
+                                        (powerline-process face2)
+                                        (powerline-raw " :" face2)
+                                        (powerline-minor-modes face2 'l)
+                                        (powerline-raw " " face2)
+                                        (funcall separator-right face2 face1)
+                                        ))
+                             )
+                        (concat (powerline-render lhs)
+                                (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                                (powerline-render center)
+                                (powerline-fill face1 (powerline-width rhs))
+                                (powerline-render rhs)))))))
+   (powerline-right-theme)
+      '';
     };
 
     treemacs-projectile = {
