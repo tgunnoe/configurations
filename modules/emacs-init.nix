5df8aed3a28924a694be91eb447cca6f5a9cf535
tgunnoe <t@gvno.net>
2020-07-08 23:00:43 -0400

    Add doom-theme

--- modules/emacs-init.nix
+++ modules/emacs-init.nix
@@ -207,7 +207,9 @@
         ''"\\.adoc\\'"''
       ];
     };
-
+    all-the-icons = {
+      enable = true;
+    };
     ansi-color = {
       enable = true;
       command = [ "ansi-color-apply-on-region" ];
@@ -309,7 +311,28 @@
           (setq doom-modeline-buffer-file-name-style 'truncate-except-project)
         '';
     };
+    doom-themes = {
+      enable = true;
+      config = ''
+             ;; Global settings (defaults)
+             (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+               doom-themes-enable-italic t) ; if nil, italics is universally disabled
+             (load-theme 'doom-one t)
 
+             ;; Enable flashing mode-line on errors
+             (doom-themes-visual-bell-config)
+
+             ;; Enable custom neotree theme (all-the-icons must be installed!)
+             (doom-themes-neotree-config)
+
+             ;; or for treemacs users
+             (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+             (doom-themes-treemacs-config)
+
+             ;; Corrects (and improves) org-mode's native fontification.
+             (doom-themes-org-config)
+      '';
+    };
     drag-stuff = {
       enable = true;
       bind = {
